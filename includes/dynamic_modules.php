cd2ed3fa55bfc4b0418a261a4ccc4b7a0bdbfef8
Anand <anandh.pgm@gmail.com>
2026-06-26 23:01:59 +0530

    Tagging field title issue fixed

--- includes/dynamic_modules.php
+++ includes/dynamic_modules.php
@@ -391,7 +391,15 @@ function dm_fetch_records(PDO $conn, string $p, int $moduleId, ?string $search =
     $valueMap = [];
     if ($recordIds) {
         $inClause = implode(',', array_map('intval', $recordIds));
-        $vStmt = $conn->query("SELECT record_id, field_id, value FROM {$p}module_record_values WHERE record_id IN ($inClause)");
+        $vStmt = $conn->query("
+            SELECT rv.record_id, rv.field_id,
+                IF(f.field_type = 'api_call_picker' AND rv.value REGEXP '^[0-9]+$', 
+                   COALESCE((SELECT value FROM {$p}module_record_values WHERE record_id = CAST(rv.value AS UNSIGNED) ORDER BY field_id ASC LIMIT 1), rv.value), 
+                   rv.value) as value
+            FROM {$p}module_record_values rv
+            JOIN {$p}module_fields f ON f.id = rv.field_id
+            WHERE rv.record_id IN ($inClause)
+        ");
         foreach ($vStmt->fetchAll(PDO::FETCH_ASSOC) as $v) {
             $valueMap[(int) $v['record_id']][(int) $v['field_id']] = $v['value'];
         }
